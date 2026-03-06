05ecb2a5979109a2060812393262a27237ba7909
Xing688 <3028653514@qq.com>
2026-03-06 11:22:31 +0800

    计数器控制LED

---
+++ CountLed_project/CountLed_project.v
@@ -0,0 +1,36 @@
+//模块名：CountLed_project
+//功能：使用计数器控制Led灯的状态，实现亮0.5秒，灭0.5秒的效果
+//说明：在计数器达到最大值时，输出信号为高电平，点亮LED灯；在其他时间为低电平，熄灭LED灯
+
+module CountLed_project#(
+    parameter Count_Max = 28'd25000000 // 计数器最大值，时钟频率为50MHz，要实现0.5秒的周期，计数器需要计数25000000个时钟周期
+) (
+    input  wire Clk,
+    input  wire Rst_n,
+    output reg  [3:0]Out_sig
+);
+
+reg [27:0] Count;
+
+//计数器复位与自增逻辑
+always @(posedge Clk or negedge Rst_n) begin
+    if (!Rst_n) begin
+        Count <= 28'd0;  // 复位时计数器清零
+    end else if (Count == Count_Max) begin
+        Count <= 28'd0;  // 计数器达到最大值时清零
+    end else  begin
+        Count <= Count + 28'd1;  // 时钟上升沿计数器加1
+    end
+end
+
+always @(posedge Clk or negedge Rst_n) begin
+    if (!Rst_n) begin
+        Out_sig <= 4'b0000;  // 复位时输出信号为低电平，LED灯点亮
+    end else if (Count == Count_Max) begin
+        Out_sig <= ~Out_sig;  // 计数器达到最大值时，翻转输出信号，点亮或熄灭LED灯
+    end else begin
+        Out_sig <= Out_sig;  // 其他时间保持当前状态
+    end
+end
+
+endmodule
---
+++ CountLed_project/CountLed_project_tb.v
@@ -0,0 +1,33 @@
+`timescale 1ns/1ps
+
+module CountLed_project_tb();
+
+reg Clk;
+reg Rst_n;
+wire [3:0] Out_sig;
+
+CountLed_project#(
+    .Count_Max(28'd25000000) // 计数器最大值，时钟频率为50MHz，要实现0.5秒的周期，计数器需要计数25000000个时钟周期
+) CountLed_project_inst(
+    .Clk(Clk),
+    .Rst_n(Rst_n),
+    .Out_sig(Out_sig)
+);
+
+initial begin
+    // 初始化信号
+    Clk = 0;
+    Rst_n = 0; // 初始状态为复位
+
+    // 释放复位信号
+    #15 Rst_n = 1; // 15ns后释放复位
+
+    // 运行仿真一段时间以观察输出
+    #1000000100; // 1s+100ns的仿真时间
+    $stop; // 停止仿真
+end
+
+// 生成时钟信号，周期为20ns（50MHz）
+always #10 Clk = ~Clk; // 每10ns翻转一次时钟信号
+
+endmodule
